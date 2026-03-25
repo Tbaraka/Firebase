9a6c610a0a6f443061311332353d361847cb2703
Tbaraka <barakateddy442@gmail.com>
2026-03-25 16:15:40 +0300

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/firebase/signup.java
+++ app/src/main/java/com/example/firebase/signup.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -21,29 +22,31 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class signup extends AppCompatActivity {
 
     private EditText emailtext, passwordtext;
     private Button registbutton;
     private ProgressBar registrProgressbar;
     private FirebaseAuth Auth;
+    private TextView loginRedirectText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
         Auth = FirebaseAuth.getInstance();
-// initialising all views through id defined above
         emailtext = findViewById(R.id.email);
         passwordtext = findViewById(R.id.password);
         registbutton = findViewById(R.id.registerbtn);
         registrProgressbar = findViewById(R.id.progressbar);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
 
         registbutton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,45 +54,43 @@ public class MainActivity extends AppCompatActivity {
                 registernewuser();
             }
         });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(signup.this, login.class);
+                startActivity(intent);
+            }
+        });
     }
 
     private void registernewuser() {
-// show the visibility of progress bar to show loading
         registrProgressbar.setVisibility(View.VISIBLE);
-        // Take the value of two edit texts in Strings
         String email, password;
         email = emailtext.getText().toString();
         password = passwordtext.getText().toString();
 
-        // Validations for input email and password
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
             registrProgressbar.setVisibility(View.GONE);
             return;
-
         }
         if (TextUtils.isEmpty(password)) {
             Toast.makeText(this, "Enter password", Toast.LENGTH_SHORT).show();
             registrProgressbar.setVisibility(View.GONE);
             return;
         }
-        // create new user or register new user
-        Auth.createUserWithEmailAndPassword(email,
-                password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+        Auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 registrProgressbar.setVisibility(View.GONE);
                 if (task.isSuccessful()) {
-                    Toast.makeText(getApplicationContext(), "Registration Succesful", Toast.LENGTH_SHORT).show();
-                    // hide the progress bar
-                    Intent intent = new Intent(MainActivity.this, login.class);
+                    Toast.makeText(getApplicationContext(), "Registration Successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(signup.this, login.class);
                     startActivity(intent);
                 } else {
-                    // Registration failed
-                    Toast.makeText(getApplicationContext(), "Failed Registration", Toast.LENGTH_SHORT).show();
-
-                    // hide the progress bar
-                    registrProgressbar.setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(), "Failed Registration: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                 }
             }
         });
